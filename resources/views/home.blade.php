51e5a12ca16b3afb3abfad06a76d664ea0e3ccdd
rifqiarrahim <80544137+rifqiarrahim@users.noreply.github.com>
2022-04-05 23:44:03 +0700

    layouting

---
+++ resources/views/home.blade.php
@@ -0,0 +1,5 @@
+@extends('templates.layout')
+@section('container')
+<h2 tabindex="0">Explore Cafe</h2>
+<div class="restaurant-list"></div>
+@endsection
---
+++ resources/views/login.blade.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>Document</title>
+    <style>
+        *,
+        *:after,
+        *:before {
+            -webkit-box-sizing: border-box;
+            box-sizing: border-box;
+            margin: 0;
+            padding: 0;
+        }
+    </style>
+</head>
+
+<body>
+
+    <body class="text-center" data-new-gr-c-s-check-loaded="14.1055.0" data-gr-ext-installed="">
+
+        <main class="form-signin">
+            <form>
+                <img class="mb-4" src="/docs/5.1/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57">
+                <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+
+                <div class="form-floating">
+                    <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+                    <label for="floatingInput">Email address</label>
+                </div>
+                <div class="form-floating">
+                    <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+                    <label for="floatingPassword">Password</label>
+                </div>
+
+                <div class="checkbox mb-3">
+                    <label>
+                        <input type="checkbox" value="remember-me"> Remember me
+                    </label>
+                </div>
+                <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+                <p class="mt-5 mb-3 text-muted">© 2017–2021</p>
+            </form>
+        </main>
+
+
+
+
+
+        <div>
+            <div id="hljs" class="hljs" style="display: none;"></div>
+            <div id="hljs-string" class="hljs-string" style="display: none;"></div>
+            <div id="hljs-keyword" class="hljs-keyword" style="display: none;"></div>
+            <div id="hljs-variable" class="hljs-variable" style="display: none;"></div>
+            <div id="hljs-comment" class="hljs-comment" style="display: none;"></div>
+        </div>
+    </body>
+</body>
+
+</html>
